d0e7ff93b5074400ca290d554d9948880f9085d8
Luke Geiken <lugeiken@microsoft.com>
2019-05-05 16:58:38 -0700

    setting stuff back to non-test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,37 @@
 import app from './app';
 const PORT = 3001;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log('Express server listening on port ' + PORT);
 });
+
+function handleExit(options: any, err: any) {
+    console.log(options.test);
+    if (options.cleanup) {
+        const actions = [
+            server.close
+        ];
+
+        actions.forEach((close, i) => {
+        try {
+            close(() => {
+            if (i === actions.length - 1) {
+                process.exit();
+            }
+          });
+        } catch (err) {
+            if (i === actions.length - 1) {
+                process.exit();
+            }
+        }
+      });
+    }
+    if (options.exit) {
+        process.exit();
+    }
+}
+
+process.on('exit', handleExit.bind(null, { cleanup: true, test: 'exit' }));
+process.on('SIGINT', handleExit.bind(null, { exit: true, test: 'SIGINT' }));
+process.on('SIGTERM', handleExit.bind(null, { exit: true, test: 'SIGTERM' }));
+process.on('uncaughtException', handleExit.bind(null, { exit: true, test: 'uncaughtException' }));
